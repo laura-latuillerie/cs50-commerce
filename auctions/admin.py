1dc87cdc9e59f91314f2a1d44f84b54d11032e67
laura-latuillerie <laura.latuillerie@gmail.com>
2022-05-07 17:09:26 +0200

    Add Category and Listing in Model + Create a Listing

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Category, Listing
 
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Listing)
+
 
--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,27 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+class Listing(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=256)
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.URLField(max_length=1024, blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default='', blank=True, null=True)
+    active = models.BooleanField(default=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    created_at = models.DateTimeField(auto_now_add=True)
+    watched_by = models.ManyToManyField(User, blank=True, related_name="watchlist")
+    
+    def __str__(self):
+        return f"{self.title}"
+    
